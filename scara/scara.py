dd532674d26962fc045812da9a16aa32f9f61667
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-08-28 04:10:29 +0530

    Initial commit

---
+++ scara/scara.py
@@ -0,0 +1,15 @@
+import pybullet as p
+
+# Initialize PyBullet simulation
+physicsClient = p.connect(p.GUI)
+p.setGravity(0, 0, -9.81)
+
+# Load the URDF file
+robot = p.loadURDF("scara.urdf", [0, 0, 0], useFixedBase=True)
+
+# Simulation loop
+while True:
+    p.stepSimulation()
+
+# Disconnect from the simulation
+# p.disconnect()
